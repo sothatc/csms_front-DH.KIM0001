d8c4a7686e80ca9f82b52a8bd4f1cf3792f2e188
sothatc <dkdltbem37@gmail.com>
2023-12-17 00:31:49 +0900

    [Fix] system info modal style Fix

--- src/components/organisms/Dialog/SystemInfoModal/SystemInfoModal.jsx
+++ src/components/organisms/Dialog/SystemInfoModal/SystemInfoModal.jsx
@@ -2,6 +2,8 @@ import { IconImage } from 'components/atoms';
 import { useDispatch } from 'react-redux';
 import { closeModal } from 'reduxStore/modalSlice';
 import styles from './SystemInfoModal.module.scss';
+import { Button } from 'components/atoms/Button/Button';
+import { Select } from 'components/atoms/Select/Select';
 
 
 
@@ -25,6 +27,139 @@ const SystemInfoModal = () => {
           <div className={styles.close} onClick={handleClose}>
             <IconImage icon={'CLOSE'} onClickEvent={handleClose} />
           </div>
+        </div>
+        <div className={styles.modal__servers}>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLV
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+          <div>
+            KRPCMPLVRM310
+            <IconImage icon={'CLOSE'} />
+          </div>
+        </div>
+        <div className={styles.modal__content}>
+          <div>
+            <div>서버 Hostname</div>
+            <input />
+          </div>
+          <div>
+            <div>서버 IP</div>
+            <input />
+          </div>
+          <div>
+            <div>서버 용도</div>
+            <input />
+          </div>
+          <div>
+            <div>Kernel</div>
+            <input />
+          </div>
+          <div>
+            <div>GPU Model</div>
+            <input />
+          </div>
+          <div>
+            <div>기본경로</div>
+            <input />
+          </div>
+          <div>
+            <div>로그경로</div>
+            <input />
+          </div>
+          <div>
+            <div>
+              <div>사용 여부</div>
+              <Select />
+            </div>
+            <div>
+              <div>학습 사용</div>
+              <Select />
+            </div>
+            <div>
+              <div>리소스 사용</div>
+              <Select />
+            </div>
+          </div>
+          <div>
+            <div>Memory</div>
+            <input />
+          </div>
+          <div>
+            <div>Disk</div>
+            <input />
+          </div>
+        </div>
+        <div className={styles.modal__btn}>
+          <Button value={'닫기'}/>
         </div>
       </div>
     </div>
